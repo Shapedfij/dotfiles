7cc0b86cf8d1b94ba8c5e44c13d6aa6870df0c5d
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-03-05 18:17:55 +0530

    🔥 prompt prefix updated in telescope.nvim

--- nvim/lua/fulies/telescope.lua
+++ nvim/lua/fulies/telescope.lua
@@ -3,8 +3,10 @@ local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
         file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
+        prompt_position = "bottom",
+        prompt_prefix = "-> ",
         color_devicons = true,
+        set_env = { ['COLORTERM'] = 'truecolor' }, -- default = nil,
 
         file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
         grep_previewer   = require('telescope.previewers').vim_buffer_vimgrep.new,
