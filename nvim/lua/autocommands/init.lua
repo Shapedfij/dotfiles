c40c3d644b28c6c76f6a20b5324cfa24bd9ad203
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-02 18:14:53 +0530

    ✌ All formatter added

--- nvim/lua/autocommands/init.lua
+++ nvim/lua/autocommands/init.lua
@@ -17,8 +17,14 @@ utils.define_augroups({
 
     -- Auto Formaters
     _auto_formatters = {
-        -- Lua
-        {'BufWrite', '*.lua', 'call LuaFormat()'}, -- Python
-        {'BufWritePre', '*.py', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'}
+        {'BufWrite', '*.lua', 'call LuaFormat()'},
+        {'BufWritePre', '*.py', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.js', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.js', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.jsx', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.ts', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.tsx', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.json', 'lua vim.lsp.buf.formatting_sync(nil, 1000)'},
+        {'BufWritePre', '*.rb', 'lua vim.lsp.buf.formatting_sync(nil,1000)'}
     }
 })
--- nvim/lua/lsp/ls-efm.lua
+++ nvim/lua/lsp/ls-efm.lua
@@ -1,7 +1,16 @@
+-- tsserver/web javascript react, vue, json, html, css, yaml
+local prettier = {
+    formatCommand = "prettier --stdin-filepath ${INPUT}",
+    formatStdin = true
+}
+
 require"lspconfig".efm.setup {
     cmd = {vim.fn.stdpath('data') .. "/lspinstall/efm/efm-langserver"},
     init_options = {documentFormatting = true},
-    filetypes = {"python"},
+    filetypes = {
+        "python", "sh", "javascript", "javascriptreact", "typescript",
+        "typescriptreact", "html", "css", "json", "yaml", "markdown"
+    },
     settings = {
         rootMarkers = {".git/"},
         languages = {
@@ -12,7 +21,29 @@ require"lspconfig".efm.setup {
                     lintFormats = {"%f:%l:%c: %m"}
                 }, {formatCommand = "isort --quiet -", formatStdin = true},
                 {formatCommand = "black --quiet -", formatStdin = true}
+            },
+            sh = {
+                LintCommand = 'shellcheck -f gcc -x',
+                lintFormats = {
+                    '%f:%l:%c: %trror: %m', '%f:%l:%c: %tarning: %m',
+                    '%f:%l:%c: %tote: %m'
+                }
+            },
+            javascript = prettier,
+            javascriptreact = prettier,
+            typescript = prettier,
+            typescriptreact = prettier,
+            html = prettier,
+            css = prettier,
+            json = prettier,
+            yaml = prettier,
+            markdown = {
+                formatCommand = 'pandoc -f markdown -t gfm -sp --tab-stop=2',
+                formatStdin = true
             }
         }
     }
 }
+
+-- Also find way to toggle format on save
+-- maybe this will help: https://superuser.com/questions/439078/how-to-disable-autocmd-or-augroup-in-vim
