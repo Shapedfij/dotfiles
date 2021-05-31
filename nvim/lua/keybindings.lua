42f45fc902922bd2cb272382fbecfe2d1e7b5af2
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-31 11:33:48 +0530

    🔧 Keybinding with Nvim api

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -1,34 +1,30 @@
-local noremap = function(v, lhs, rhs)
-  vim.api.nvim_set_keymap(v, lhs, rhs, {noremap = true})
-end
-
 -- Leader key
-noremap("n", "<Space>", "<Nop>")
+vim.api.nvim_set_keymap("n", "<Space>", "<Nop>", {noremap = true})
 vim.g.mapleader = " "
 
+-- Quickfix list
+vim.api.nvim_set_keymap("n", "]q", ":cnext<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "[q", ":cprev<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "\\q", ":clist<CR>", {noremap = true})
+
 -- Line transform using 'Alt'
-noremap("n", "<A-j>", ":m .+1<CR>==")
-noremap("n", "<A-k>", ":m .-2<CR>==")
-noremap("i", "<A-j>", "<Esc>:m .+1<CR>==gi")
-noremap("i", "<A-k>", "<Esc>:m .-2<CR>==gi")
-noremap("v", "<A-j>", ":m '>+1<CR>gv=gv")
-noremap("v", "<A-k>", ":m '<-2<CR>gv=gv")
+vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", {noremap = true})
+vim.api.nvim_set_keymap("n", "<A-k>", ":m .-2<CR>==", {noremap = true})
+vim.api.nvim_set_keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", {noremap = true})
+vim.api.nvim_set_keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", {noremap = true})
+vim.api.nvim_set_keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", {noremap = true})
+vim.api.nvim_set_keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", {noremap = true})
 
 -- Tab navigation
-noremap("n", "<Leader><Esc>", ":bdelete<CR>")
-noremap("n", "<Leader>]", ":bnext<CR>")
-noremap("n", "<Leader>[", ":bprev<CR>")
+vim.api.nvim_set_keymap("n", "<Leader><Esc>", ":bdelete<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>]", ":bnext<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>[", ":bprev<CR>", {noremap = true})
 
 -- Window navigation
-noremap("n", "<Leader>h", ":wincmd h<CR>")
-noremap("n", "<Leader>j", ":wincmd j<CR>")
-noremap("n", "<Leader>k", ":wincmd k<CR>")
-noremap("n", "<Leader>l", ":wincmd l<CR>")
-
--- Quickfix list
-noremap("n", "<C-]>", ":cnext<CR>")
-noremap("n", "<C-[>", ":cprev<CR>")
-noremap("n", "<C-\\>", ":copen<CR>")
+vim.api.nvim_set_keymap("n", "<Leader>h", ":wincmd h<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>j", ":wincmd j<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>k", ":wincmd k<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>l", ":wincmd l<CR>", {noremap = true})
 
 -- Clear search result
-noremap("n", "<C-n>", ":set hlsearch!<CR>")
+vim.api.nvim_set_keymap("n", "<C-n>", ":set hlsearch!<CR>", {noremap = true})
