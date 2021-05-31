2fcf39be8015b6355aba3685bf727bf23b0420c0
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-05-31 11:34:19 +0530

    👔 Gruvbox-flat

--- nvim/lua/visual.lua
+++ nvim/lua/visual.lua
@@ -1,17 +1,17 @@
--- Gruvbox
-vim.cmd([[colorscheme gruvbox]])
-vim.o.background = "dark" -- or "light" for light mode
-vim.g.gruvbox_transparent_bg = 1
+-- Gruvbox Flat
+vim.g.gruvbox_dark_sidebar = false
+vim.g.gruvbox_transparent = true
+vim.g.gruvbox_italic_functions = true
+vim.g.gruvbox_hide_inactive_statusline = true
+vim.g.gruvbox_dark_float = false
+vim.g.gruvbox_colors = {hint = "orange"}
 
--- Keyword bold
-vim.g.ci_dark_enable_bold = 1
+vim.cmd [[colorscheme gruvbox-flat]]
 
 -- Transparent background
 vim.cmd("highlight Normal     ctermbg=NONE guibg=NONE")
 vim.cmd("highlight LineNr     ctermbg=NONE guibg=NONE")
 vim.cmd("highlight SignColumn ctermbg=NONE guibg=NONE")
-
-vim.cmd("highlight HighlightedyankRegion cterm=reverse gui=reverse")
 
 -- plugins colorizer
 require "colorizer".setup()
