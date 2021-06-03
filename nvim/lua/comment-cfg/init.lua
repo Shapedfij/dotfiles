a7fde997dc7085e5039fbf8012ef64304e9632b8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-03 11:03:13 +0530

    ✨ Make prettier

--- nvim/lua/comment-cfg/init.lua
+++ nvim/lua/comment-cfg/init.lua
@@ -1,14 +1,10 @@
 require("nvim_comment").setup(
   {
-    -- Linters prefer comment and line to have a space in between markers
-    marker_padding = true,
-    -- should comment out empty or whitespace only lines
-    comment_empty = true,
-    -- Should key mappings be created
+    comment_empty = false,
     create_mappings = true,
     -- Normal mode mapping left hand side
-    line_mapping = "<Leader>c",
+    line_mapping = "<space>/",
     -- Visual/Operator mapping left hand side
-    operator_mapping = "<Leader>/"
+    operator_mapping = "<space>//"
   }
 )
