34352b1c98823f5b4b0e448c6e482e9d10cbf4d5
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-04 19:06:22 +0530

    ✨ More visual changes inside nvimtree

--- nvim/lua/nvimtree-cfg/init.lua
+++ nvim/lua/nvimtree-cfg/init.lua
@@ -2,9 +2,13 @@ vim.g.nvim_tree_side = "right"
 vim.g.nvim_tree_width = 30
 vim.g.nvim_tree_disable_netrw = 0 -- "1 by default, disables netrw
 vim.g.nvim_tree_auto_ignore_ft = "startify" -- empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_highlight_opened_files = 1
 vim.g.nvim_tree_indent_markers = 1
+vim.g.nvim_tree_disable_netrw = 0
+vim.g.nvim_tree_lsp_diagnostics = 1
 vim.g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1}
 
 require("nvim-tree.events").on_nvim_tree_ready(
