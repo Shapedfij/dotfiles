bfe00847336917881ee549c3772e3a60f9597f11
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-05 16:09:50 +0530

    🔥 Icons added

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -79,15 +79,48 @@ end
 -- SpellChecker
 require("spellsitter").setup()
 
+-- Set Default Prefix.
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics,
   {
     virtual_text = {
-      prefix = "● "
-    }
+      prefix = "",
+      spacing = 0
+    },
+    signs = true,
+    underline = true
   }
 )
+
+-- symbols for autocomplete
+vim.lsp.protocol.CompletionItemKind = {
+  "   (Text) ",
+  "   (Method)",
+  "   (Function)",
+  "   (Constructor)",
+  " ﴲ  (Field)",
+  "[] (Variable)",
+  "   (Class)",
+  " ﰮ  (Interface)",
+  "   (Module)",
+  " 襁 (Property)",
+  "   (Unit)",
+  "   (Value)",
+  " 練 (Enum)",
+  "   (Keyword)",
+  "   (Snippet)",
+  "   (Color)",
+  "   (File)",
+  "   (Reference)",
+  "   (Folder)",
+  "   (EnumMember)",
+  " ﲀ  (Constant)",
+  " ﳤ  (Struct)",
+  "   (Event)",
+  "   (Operator)",
+  "   (TypeParameter)"
+}
 require("lsp-colors").setup(
   {
     Error = "#db4b4b",
