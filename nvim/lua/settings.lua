9241eecfe4f7499011f21cf9aa0cb0b674612bf4
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-06-07 14:49:29 +0530

    ⚙ Termguicolors setting

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,7 +1,7 @@
 vim.cmd("syntax on") -- syntax highlighting
--- vim.o.spell = true
--- vim.o.termguicolor = true -- set term gui colors most terminals support this
--- vim.o.t_Co = "256" -- Support 256 colors
+vim.g.termguicolors = true -- set term gui colors most terminals support this
+vim.g.t_Co = 256 -- Support 256 colors
+
 vim.o.hidden = true -- Required to keep multiple buffers open multiple buffers
 vim.o.pumheight = 10 -- Makes popup menu smaller
 
