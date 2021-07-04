73272856a7948f77048f9f70322f9b77bec928a1
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-07-04 19:25:41 +0530

    ⬆️ sync

--- nvim/.config/nvim/lua/plugins/plug.lua
+++ nvim/.config/nvim/lua/plugins/plug.lua
@@ -4,74 +4,74 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " ..
-                install_path)
-    execute "packadd packer.nvim"
+  execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+  execute "packadd packer.nvim"
 end
 
 vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 
 return require("packer").startup(function(use)
-    -- Packer can manage itself as an optional plugin
-    use "wbthomason/packer.nvim"
+  -- Packer can manage itself as an optional plugin
+  use "wbthomason/packer.nvim"
 
-    -- Native LSP
-    use "neovim/nvim-lspconfig"
-    use "kabouzeid/nvim-lspinstall"
-    use "mattn/efm-langserver"
-    use "folke/lsp-colors.nvim"
-    use "windwp/nvim-ts-autotag"
+  -- Native LSP
+  use "neovim/nvim-lspconfig"
+  use "kabouzeid/nvim-lspinstall"
+  use "mattn/efm-langserver"
+  use "folke/lsp-colors.nvim"
+  use "windwp/nvim-ts-autotag"
 
-    -- Spell Checker
-    use "lewis6991/spellsitter.nvim"
+  -- Spell Checker
+  use "lewis6991/spellsitter.nvim"
 
-    -- Auto Completion
-    use "hrsh7th/nvim-compe"
-    use "hrsh7th/vim-vsnip"
-    use "rafamadriz/friendly-snippets"
+  -- Auto Completion
+  use "hrsh7th/nvim-compe"
+  use "hrsh7th/vim-vsnip"
+  use "rafamadriz/friendly-snippets"
 
-    -- Telescope
-    use "nvim-lua/plenary.nvim"
-    use "nvim-telescope/telescope.nvim"
-    use "nvim-lua/popup.nvim"
-    use "nvim-telescope/telescope-fzy-native.nvim"
+  -- Telescope
+  use "nvim-lua/plenary.nvim"
+  use "nvim-telescope/telescope.nvim"
+  use "nvim-lua/popup.nvim"
+  use "nvim-telescope/telescope-fzy-native.nvim"
 
-    -- Treesitter
-    use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+  -- Treesitter
+  use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
+  use "nvim-treesitter/playground"
 
-    -- Git
-    use "tpope/vim-fugitive"
-    use "lewis6991/gitsigns.nvim"
+  -- Git
+  use "tpope/vim-fugitive"
+  use "lewis6991/gitsigns.nvim"
 
-    -- Explorer
-    use "kyazdani42/nvim-tree.lua"
-    use "kevinhwang91/rnvimr"
+  -- Explorer
+  use "kyazdani42/nvim-tree.lua"
+  use "kevinhwang91/rnvimr"
 
-    -- Flutter
-    use {"akinsho/flutter-tools.nvim", requires = "nvim-lua/plenary.nvim"}
-    use "dart-lang/dart-vim-plugin"
-    use "Neevash/awesome-flutter-snippets"
+  -- Flutter
+  use {"akinsho/flutter-tools.nvim", requires = "nvim-lua/plenary.nvim"}
+  use "dart-lang/dart-vim-plugin"
+  use "Neevash/awesome-flutter-snippets"
 
-    -- ColorScheme
-    use {"ful1e5/onedark.nvim", branch = "dev"}
+  -- ColorScheme
+  use {"ful1e5/onedark.nvim", branch = "dev"}
 
-    -- Color previewers
-    use "norcalli/nvim-colorizer.lua"
+  -- Color previewers
+  use "norcalli/nvim-colorizer.lua"
 
-    -- Icons
-    use "kyazdani42/nvim-web-devicons"
+  -- Icons
+  use "kyazdani42/nvim-web-devicons"
 
-    -- Status Line
-    use "hoob3rt/lualine.nvim"
+  -- Status Line
+  use "hoob3rt/lualine.nvim"
 
-    -- Formatter
-    use "mhartington/formatter.nvim"
-    use "andrejlevkovitch/vim-lua-format"
-    use {"prettier/vim-prettier", run = "yarn install"}
-    use "psf/black"
+  -- Formatter
+  use "mhartington/formatter.nvim"
+  use "andrejlevkovitch/vim-lua-format"
+  use {"prettier/vim-prettier", run = "yarn install"}
+  use "psf/black"
 
-    -- Utilies Plugins
-    use {"tpope/vim-surround"}
-    use "terrortylor/nvim-comment"
-    use "windwp/nvim-autopairs"
+  -- Utilies Plugins
+  use {"tpope/vim-surround"}
+  use "terrortylor/nvim-comment"
+  use "windwp/nvim-autopairs"
 end)
