239e13a0962219da0e108e419fe76aad2c866a0e
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-08-21 16:24:31 +0530

    ☁️ git pull & git push mapping init

--- nvim/.config/nvim/lua/plugins-cfg/fugitive/init.lua
+++ nvim/.config/nvim/lua/plugins-cfg/fugitive/init.lua
@@ -1,7 +1,10 @@
 -- Git
 -- NOTE: 'dv' to get conflicts
+-- NOTE: Ctrl+w & Ctrl+o to save
 vim.api.nvim_set_keymap("n", "<Leader>gh", ":diffget //2<CR>", {noremap = true})
 vim.api.nvim_set_keymap("n", "<Leader>gl", ":diffget //3<CR>", {noremap = true})
 
 -- Fugitive
 vim.api.nvim_set_keymap("n", "<Leader>gs", ":Gstatus<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>gp", ":G push<CR>", {noremap = true})
+vim.api.nvim_set_keymap("n", "<Leader>gl", ":G pull<CR>", {noremap = true})
