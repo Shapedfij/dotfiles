ce4bf9c65dffb712eace6a19f5159280b8270dd3
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-08-26 17:55:38 +0530

    ⚡ project_files func added

--- nvim/.config/nvim/lua/config/telescope/init.lua
+++ nvim/.config/nvim/lua/config/telescope/init.lua
@@ -35,22 +35,25 @@ M.search_kitty_dotfiles = function()
   })
 end
 
+M.project_files = function()
+  local opt = {} -- define here if you want to define something
+  local ok = pcall(require"telescope.builtin".git_files, opt)
+  if not ok then require"telescope.builtin".find_files(opt) end
+end
+
 -- Custom KeyBinding
 local opts = {noremap = true, silent = true}
-
 vim.api.nvim_set_keymap("n", "<Leader>vrc", [[<cmd>lua require('config.telescope').search_dotfiles()<cr>]], opts)
 vim.api.nvim_set_keymap("n", "<Leader>term", [[<cmd>lua require('config.telescope').search_kitty_dotfiles()<cr>]], opts)
+vim.api.nvim_set_keymap("n", "<C-p>", [[<cmd>lua require('config.telescope').project_files()<cr>]], opts)
 
+-- Builtin KeyBinding
 vim.api.nvim_set_keymap("n", "<Leader>help", [[<cmd>lua require('telescope.builtin').help_tags()<cr>]], opts)
 vim.api.nvim_set_keymap("n", "<Leader>man", [[<cmd>lua require('telescope.builtin').man_pages()<cr>]], opts)
 vim.api.nvim_set_keymap("n", "<Leader>=", [[<cmd>lua require('telescope.builtin').spell_suggest()<cr>]], opts)
 
 vim.api.nvim_set_keymap("n", "<Leader>ca", [[<cmd>lua require('telescope.builtin').lsp_code_actions()<cr>]], opts)
 vim.api.nvim_set_keymap("n", "<Leader>ts", [[<cmd>lua require('telescope.builtin').treesitter()<cr>]], opts)
-
-vim.api.nvim_set_keymap("n", "<C-p>", [[<cmd>lua require('telescope.builtin').git_files()<cr>]], opts)
-vim.api.nvim_set_keymap("n", "<Leader>p", [[<cmd>lua require('telescope.builtin').find_files()<cr>]], opts)
-vim.api.nvim_set_keymap("n", "<Leader><Tab>", [[<cmd>lua require('telescope.builtin').buffers()<cr>]], opts)
 
 vim.api.nvim_set_keymap("n", "<Leader>pf",
                         [[<cmd>lua require('telescope.builtin').grep_string({search = vim.fn.input("Grep For > ")})<cr>]],
