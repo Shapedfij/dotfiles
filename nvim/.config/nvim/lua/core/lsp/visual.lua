1dec3b11e88fc0721ba414233b642abbb2a94489
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-08-30 14:45:32 +0530

    📁 LSP visual settings structured

--- nvim/.config/nvim/lua/core/lsp/visual.lua
+++ nvim/.config/nvim/lua/core/lsp/visual.lua
@@ -1,3 +1,6 @@
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = M.border})
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = M.border})
+
 -- lsp diagnostics text
 vim.fn.sign_define("LspDiagnosticsSignError",
                    {texthl = "LspDiagnosticsSignError", numhl = "LspDiagnosticsSignError", text = " "})
