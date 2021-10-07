84fd40c816bebbb9f8ad62bddd674fa6821cc579
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-07 18:34:40 +0530

    ⚡ dev config

--- nvim/.config/nvim/lua/themes/github.lua
+++ nvim/.config/nvim/lua/themes/github.lua
@@ -1,6 +1,9 @@
--- require("lualine").setup({options = {theme = "github"}})
+require("lualine").setup({options = {theme = "github"}})
 require("github-theme").setup({
-  -- theme_style = "dark",
+  -- theme_style = "light",
+  -- theme_style = "light_default",
+  -- theme_style = "dark_default",
+  -- theme_style = "dimmed",
   transparent = false,
   dark_sidebar = false,
   colors = {bg_search = "green", fg_search = "black"}
