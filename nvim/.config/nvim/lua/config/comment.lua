165f2afdcd38a815f59adc544c7159b8e3b852f0
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-11 17:31:23 +0530

    re-map: comment bindings

--- nvim/.config/nvim/lua/config/comment.lua
+++ nvim/.config/nvim/lua/config/comment.lua
@@ -2,7 +2,7 @@ require("nvim_comment").setup({
   comment_empty = false,
   create_mappings = true,
   -- Normal mode mapping left hand side
-  line_mapping = "<Leader>//",
+  line_mapping = "<Leader>/",
   -- Visual/Operator mapping left hand side
-  operator_mapping = "<Leader>/"
+  operator_mapping = "<Leader>//"
 })
