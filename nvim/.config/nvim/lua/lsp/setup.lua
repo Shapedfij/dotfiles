e7a3efecabe60b0170e406a02ad7d881b83a37f8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-17 14:49:00 +0530

    setup lsp-installer

--- nvim/.config/nvim/lua/lsp/setup.lua
+++ nvim/.config/nvim/lua/lsp/setup.lua
@@ -1,62 +1,17 @@
-require("lspinstall").setup()
-
 M = {}
 
 M.lspconfig = require("lspconfig")
-M.border = "rounded"
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-M.on_attach = function(_, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  -- Mappings.
-  local opts = {noremap = true, silent = true}
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-  buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>", opts)
-  buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", opts)
-  buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-  buf_set_keymap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border=M.border})<cr>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts={border=M.border}})<cr>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts={border=M.border}})<cr>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", opts)
-  buf_set_keymap("n", "<space>ff", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
-
-  vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
-
-  print("LSP Attached.")
-end
-
--- M.capabilities = vim.lsp.protocol.make_client_capabilities()
--- M.capabilities.textDocument.completion.completionItem.snippetSupport = true
--- M.capabilities.textDocument.completion.completionItem.resolveSupport =
---     {properties = {"documentation", "detail", "additionalTextEdits"}}
-
-M.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- LSP setup
-M.servers = require("lspinstall").installed_servers()
 local ignore = {efm = true, sumneko_lua = true}
 
-for _, lsp in pairs(M.servers) do
-  if ignore[lsp] ~= true then M.lspconfig[lsp].setup {on_attach = M.on_attach, capabilities = M.capabilities} end
-end
+local lsp_installer = require("nvim-lsp-installer")
+lsp_installer.on_server_ready(function(server)
+  local default_opts = require("lsp.opts")
+  if ignore[server] ~= true then
+    server:setup(default_opts)
+    vim.cmd [[ do User LspAttachBuffers ]]
+  end
+end)
 
 return M
