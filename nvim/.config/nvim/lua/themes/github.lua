0f8cc691d08dfcef275914646d776cf2ec6933c7
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-19 14:44:54 +0530

    font: 'Noto Sans Mono'

--- nvim/.config/nvim/lua/themes/github.lua
+++ nvim/.config/nvim/lua/themes/github.lua
@@ -1,4 +1,4 @@
--- vim.o.background = "dark"
+-- vim.o.background = "light"
 -- require("lualine").setup({options = {theme = "github"}})
 require("github-theme").setup({
   -- theme_style = "light",
