67fa86d3b0fdd0ad4a1120d9f3eb2375472a1af8
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-10-19 17:38:37 +0530

    sumneko_lua option inside lsp-installer setup

--- nvim/.config/nvim/lua/lsp/setup.lua
+++ nvim/.config/nvim/lua/lsp/setup.lua
@@ -1,8 +1,74 @@
 -- LSP installer setup
 local lsp_installer = require("nvim-lsp-installer")
-local lsp_opts = require("lsp.opts")
 
+local servers = {"bashls", "cssls", "jsonls", "pyright", "html", "yamlls", "lemminx", "tsserver", "sumneko_lua"}
+
+-- Automatic Installation
+for _, name in pairs(servers) do
+  local ok, server = lsp_installer.get_server(name)
+  -- Check that the server is supported in nvim-lsp-installer
+  if ok then
+    if not server:is_installed() then
+      print("[nvim-lsp-installer]: Installing " .. name .. " ...")
+      server:install()
+    end
+  end
+end
+
+-- Configuration
 lsp_installer.on_server_ready(function(server)
-  server:setup(lsp_opts)
-  vim.cmd [[ do User LspAttachBuffers ]]
+  local default_opts = require("lsp.opts")
+
+  -- Now we'll create a server_opts table where we'll specify our custom LSP server configuration
+  local server_opts = {
+    ["sumneko_lua"] = function()
+      local system_name
+      if vim.fn.has("mac") == 1 then
+        system_name = "macOS"
+      elseif vim.fn.has("unix") == 1 then
+        system_name = "Linux"
+      elseif vim.fn.has("win32") == 1 then
+        system_name = "Windows"
+      else
+        print("Unsupported system for sumneko")
+      end
+
+      -- set the path to the sumneko installation
+      local sumneko_root_path = vim.fn.stdpath("data") .. "/lsp_servers/sumneko_lua/extension/server"
+      local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+
+      local runtime_path = vim.split(package.path, ";")
+      table.insert(runtime_path, "lua/?.lua")
+      table.insert(runtime_path, "lua/?/init.lua")
+
+      default_opts.cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"}
+      default_opts.settings = {
+        Lua = {
+          runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+            -- Setup your lua path
+            path = runtime_path
+          },
+          diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = {"vim"}
+          },
+          workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+            maxPreload = 2000,
+            preloadFileSize = 500
+          },
+          -- Do not send telemetry data containing a randomized but unique identifier
+          telemetry = {enable = false}
+        }
+      }
+    end
+  }
+
+  -- We check to see if any custom server_opts exist for the LSP server, if so, load them, if not, use our default_opts
+  server:setup(server_opts[server.name] and server_opts[server.name]() or default_opts)
+  vim.cmd([[ do User LspAttachBuffers ]])
 end)
