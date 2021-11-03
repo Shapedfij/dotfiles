6ec81a801b3975f35c33f4d7bfdbb0f05a9b608e
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-11-03 10:39:43 +0530

    nvim (lsp): diagnostics with `vim.diagnotics`

--- nvim/.config/nvim/lua/lsp/opts.lua
+++ nvim/.config/nvim/lua/lsp/opts.lua
@@ -2,9 +2,7 @@ M = {
   -- Use an on_attach function to only map the following keys
   -- after the language server attaches to the current buffer
   on_attach = function(_, bufnr)
-
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {border = "rounded"})
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {border = "rounded"})
+    require("lsp.custom_func")
 
     local function buf_set_keymap(...)
       vim.api.nvim_buf_set_keymap(bufnr, ...)
@@ -29,12 +27,13 @@ M = {
     buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>", opts)
     buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>", opts)
     buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-    buf_set_keymap("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    buf_set_keymap("n", "<F2>", "<cmd>lua _G.rename()<cr>", opts)
     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({border = 'rounded'})<cr>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev({popup_opts={border = 'rounded'}})<cr>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next({popup_opts={border = 'rounded'}})<cr>", opts)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<cr>", opts)
+    buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.show_line_diagnostics({float={border='rounded'}})<cr>",
+                   opts)
+    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev({float={border='rounded'}})<cr>", opts)
+    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next({float={border='rounded'}})<cr>", opts)
+    buf_set_keymap("n", "<space>q", "<cmd>lua vim.diagnostic.set_loclist()<cr>", opts)
     buf_set_keymap("n", "<space>ff", "<cmd>lua vim.lsp.buf.formatting()<cr>", opts)
 
     vim.cmd("setlocal omnifunc=v:lua.vim.lsp.omnifunc")
