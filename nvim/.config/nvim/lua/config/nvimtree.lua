e51d2ad3a705bb51fdcdf33813fe61323873dd08
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-11-28 14:15:33 +0530

    nvimtree: temporary supress error

--- nvim/.config/nvim/lua/config/nvimtree.lua
+++ nvim/.config/nvim/lua/config/nvimtree.lua
@@ -1,4 +1,3 @@
-vim.g.nvim_tree_git_hl = 1
 vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_highlight_opened_files = 1
 
@@ -7,7 +6,7 @@ vim.g.nvim_tree_quit_on_open = 1
 vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_root_folder_modifier = ":p:~"
 
-require("nvim-tree").setup({diagnostics = {enable = true}, tab_open = true, auto_close = false, view = {width = 24}})
+require("nvim-tree").setup({diagnostics = {enable = false}, tab_open = true, auto_close = false, view = {width = 24}})
 
 -- Mappings
 local map = require("util").map
