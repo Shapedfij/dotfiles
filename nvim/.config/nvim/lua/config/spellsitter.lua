269e131f57f3a746b1bbf7251f1b238cda0537c4
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-12-03 16:03:21 +0530

    nvim: spellsitter config updated

--- nvim/.config/nvim/lua/config/spellsitter.lua
+++ nvim/.config/nvim/lua/config/spellsitter.lua
@@ -1,6 +1,15 @@
 require("spellsitter").setup {
+  -- Whether enabled, can be a list of filetypes, e.g. {'python', 'lua'}
+  enable = true,
+
+  -- Highlight to use for bad spellings
   hl = "SpellBad",
-  captures = {} -- set to {} to spellcheck everything
+
+  -- Spellchecker to use. values:
+  -- * vimfn: built-in spell checker using vim.fn.spellbadword()
+  -- * ffi: built-in spell checker using the FFI to access the
+  --   internal spell_check() function
+  spellchecker = "vimfn"
 }
 
 vim.spell = true
