3f5f29fae0ef80b186d1472069cb6022dcc92010
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-12-03 16:02:44 +0530

    use github dark as default theme

--- nvim/.config/nvim/lua/themes/github.lua
+++ nvim/.config/nvim/lua/themes/github.lua
@@ -2,7 +2,7 @@
 require("github-theme").setup({
   -- theme_style = "light",
   -- theme_style = "light_default",
-  theme_style = "dark_default",
+  -- theme_style = "dark_default",
   -- theme_style = "dimmed",
   transparent = false,
   dark_sidebar = false,
