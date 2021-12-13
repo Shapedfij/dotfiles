7357fdeb4fca48150784bba921ed8e43daaeb29c
ful1e5 <24286590+ful1e5@users.noreply.github.com>
2021-12-13 20:35:19 +0530

    now i am official part of RIGHT SIDE GANG

--- nvim/.config/nvim/lua/config/nvimtree.lua
+++ nvim/.config/nvim/lua/config/nvimtree.lua
@@ -10,7 +10,7 @@ require("nvim-tree").setup({
   diagnostics = {enable = true},
   tab_open = true,
   auto_close = false,
-  view = {width = 24},
+  view = {width = 24, side = "right"},
   git = {enable = true, ignore = false, timeout = 500}
 })
 
